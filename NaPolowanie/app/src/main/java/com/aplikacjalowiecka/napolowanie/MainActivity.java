4584ab9fa16bb608cb6668c6fff1a658a46d10ca
Piotr Kociolek <piotrkociolek2002@gmail.com>
2022-11-30 13:58:43 +0100

    usuniecie starych plikow aparatu

--- NaPolowanie/app/src/main/java/com/aplikacjalowiecka/napolowanie/MainActivity.java
+++ NaPolowanie/app/src/main/java/com/aplikacjalowiecka/napolowanie/MainActivity.java
@@ -1,58 +1,32 @@
 package com.aplikacjalowiecka.napolowanie;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
-    private ImageView imageView;
-    private Button button;
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(photo);
-    }
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout linear=(LinearLayout)findViewById(R.id.linear);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.caputreImage);
-        button = findViewById(R.id.OpenCamera);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(open_camera,100);
-
-            }
-        });
 
 
         Button button = findViewById(R.id.viewPdf);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), WebActivity.class);
-                intent.putExtra("pdf_url","https://www.soundczech.cz/temp/lorem-ipsum.pdf"); //plan polowan
-                startActivity(intent);
+            public void onClick(View v) {
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), WebActivity.class);
+                    intent.putExtra("pdf_url", "https://www.soundczech.cz/temp/lorem-ipsum.pdf");
+                    startActivity(intent);
 
-            }
-        });
-
+                }
+            });
+        }
+    }
 
 
     }
